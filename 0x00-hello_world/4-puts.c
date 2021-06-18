df176baa765dd265110acefa24486755085a7a70
Rayd Trujillo <rayd1893@gmail.com>
2021-06-17 20:22:04 -0700

    Print message with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Print a message with puts
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
