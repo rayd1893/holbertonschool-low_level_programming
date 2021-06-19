21c80e7ac6147d097fc3751473e5fd641af3abb0
Rayd Trujillo <rayd1893@gmail.com>
2021-06-18 20:46:26 -0700

    Print abc in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Print tebalpha
+ */
+int main(void)
+{
+	int i = 122;
+
+	while (i >= 97)
+	{
+		putchar(i);
+		i--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
