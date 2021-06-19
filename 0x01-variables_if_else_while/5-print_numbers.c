aa36a9d7f30be16ddd2ee9c373dcc4d15bcad352
Rayd Trujillo <rayd1893@gmail.com>
2021-06-18 18:40:40 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Print numbers
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+
+	printf("\n");
+
+	return (0);
+}
