9d7fdcf5ef75c149318052e0ec88d35debaa4a1e
Rayd Trujillo <rayd1893@gmail.com>
2021-06-22 13:05:05 -0700

    Print with function

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,24 @@
+#include "_putchar.h"
+
+/**
+ * main - Main function
+ *
+ * Return: Print string
+ */
+int main(void)
+{
+	int i;
+	int n;
+	char *texto = "Holberton";
+
+	n = sizeof(texto);
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(texto[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
