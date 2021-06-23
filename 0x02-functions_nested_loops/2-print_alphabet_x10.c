8d85b13c5ff13b992cacb01d19a1a19c66d89c71
Rayd Trujillo <rayd1893@gmail.com>
2021-06-22 18:46:25 -0700

    Alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - Print alphabet
+ *
+ * Return: Alphabet x 10
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+
+		_putchar(10);
+		i = 97;
+
+	}
+
+}
