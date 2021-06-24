6aa556806c112191c66a90a463b4c3adade18286
Rayd Trujillo <rayd1893@gmail.com>
2021-06-23 21:29:29 -0700

    Print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Print to 98
+ * @n: Value to evaluate
+ *
+ * Return: Print 98
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d\n", i);
+			}
+		}
+
+	}
+
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			}
+			else
+			{
+				printf("%d\n", i);
+			}
+		}
+	}
+
+}
