1d1e4d7c75b41a77672c7cf8971fe8ee7a12e717
Rayd Trujillo <rayd1893@gmail.com>
2021-06-24 23:01:16 -0700

    Print numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Print numbers
+ *
+ * Return: numbers
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar(10);
+}
