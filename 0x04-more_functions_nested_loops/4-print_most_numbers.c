fa768d34a2e5e1c8f578ec54253801e88f60e5be
Rayd Trujillo <rayd1893@gmail.com>
2021-06-24 23:29:54 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - Print numbers
+ *
+ * Return: numbers except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+	}
+
+	_putchar(10);
+}
