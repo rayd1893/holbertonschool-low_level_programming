1f065fcb5c4816278e9b5fe1f0790227768ad28a
Rayd Trujillo <rayd1893@gmail.com>
2021-06-25 01:27:44 -0700

    Print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * print_diagonal - Print diagonal
+ * @n: value
+ * Return: Diagonal
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+
+		for (i = 1; i <= n ; i++)
+		{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('/');
+			_putchar(10);
+		}
+
+	}
+
+	else
+	{
+		_putchar(10);
+	}
+
+}
