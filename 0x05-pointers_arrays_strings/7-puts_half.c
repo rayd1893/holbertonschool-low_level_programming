f0bbab4727e1e590f9f62f05a37aac647b25fdd4
Rayd Trujillo <rayd1893@gmail.com>
2021-06-29 18:20:39 +0000

    Print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include "holberton.h"
+
+/**
+ * puts_half - Return half
+ * @str: String
+ *
+ * Return: Half
+ */
+void puts_half(char *str)
+{
+	int i;
+	int l = strlen(str);
+
+	for (i = 0; i < l; i++)
+	{
+		if (i % 2 == 0)
+		{
+			if (i >= l / 2)
+			{
+				_putchar(str[i]);
+			}
+		}
+		else
+		{
+			if (i >= (l - 1) / 2)
+			{
+				_putchar(str[i]);
+			}
+		}
+
+	}
+
+	_putchar('\n');
+}
