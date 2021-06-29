9c652af8fe79418c2ff68f9fe6f5c6cdef8b7fc2
Rayd Trujillo <rayd1893@gmail.com>
2021-06-29 03:55:23 +0000

    Length string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Return string of length
+ * @s: String
+ *
+ * Return: string of length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+
+	}
+
+	return (i);
+}
