d08842efe582262956f490959683dae21e5e8b80
Rayd Trujillo <rayd1893@gmail.com>
2021-07-02 04:40:17 +0000

    Compare string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,11 +5,16 @@
  *
  * Return: int
  */
-int _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char s2)
 {
-	if (s1 == s2)
+	while (s1 == s2)
 	{
-		return (0);
+		if (s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
 	}
 	return (*s1 - *s2);
 }
