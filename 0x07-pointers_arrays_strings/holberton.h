883a5261f80192d0e4db1d06051e1f1483f20d7e
Rayd Trujillo <rayd1893@gmail.com>
2021-07-06 04:43:21 +0000

    memset

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,8 @@
+#ifndef FILE_HOLBERTON
+#define FILE_HOLBERTON
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+#endif
