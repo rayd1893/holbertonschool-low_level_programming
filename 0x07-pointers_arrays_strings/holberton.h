c870d12b62e19e98fe5098c2a54c24c62a629e1a
Rayd Trujillo <rayd1893@gmail.com>
2021-07-06 08:03:06 +0000

    searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -10,4 +10,5 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
 #endif
