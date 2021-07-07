a1beba8a5a52a350d60edee0128fe2cbf9fb3557
Rayd Trujillo <rayd1893@gmail.com>
2021-07-07 19:38:49 +0000

    Print String

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _puts_recursion - Print string
+ * @s: String
+ * Return: Print string
+ */
+void _puts_recursion(char *s)
+{
+	if (!*s)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
