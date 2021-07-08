118f0bb1ce65c244c68316fc43c04f5c628fd1b8
Rayd Trujillo <rayd1893@gmail.com>
2021-07-08 00:28:40 +0000

    Pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+/**
+ * _pow_recursion - Power
+ * @x: number
+ * @y: power
+ * Return: Power of number
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
