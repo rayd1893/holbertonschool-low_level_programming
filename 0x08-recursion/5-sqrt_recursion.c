c3b31e9926987bdd69c9e685b803ddf1cd400b14
Rayd Trujillo <rayd1893@gmail.com>
2021-07-08 04:31:58 +0000

    Square

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+/**
+ * sqrt_find - Iterator
+ * @n: Number
+ * @i: Iterator
+ * Return: square
+ */
+int sqrt_find(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+
+	if (i * i > n)
+	{
+		return (-1);
+	}
+
+	return (sqrt_find(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - Square
+ * @n: Number
+ * Return: square
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_find(n,1));
+}
