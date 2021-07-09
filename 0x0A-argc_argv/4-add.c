404252b1c9f42e0d841ed331f972b0d92cb1d355
Rayd Trujillo <rayd1893@gmail.com>
2021-07-09 09:00:45 +0000

    Sum

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Main function
+ * @argc: number of arguments
+ * @argv: Array of arguments
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int sum = 0;
+	*argv[0] = '1';
+
+	while (argc--)
+	{
+		if (atoi(*argv) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(*argv++);
+	}
+
+	printf("%d\n", sum - 1);
+
+	exit(EXIT_SUCCESS);
+}
