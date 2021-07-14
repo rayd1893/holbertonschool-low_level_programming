2ca384bf86448356d6e4cf71c8b7928720dca2fa
Rayd Trujillo <rayd1893@gmail.com>
2021-07-14 03:02:18 +0000

    Print string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - Return string
+ * @str: string
+ * Return: String
+ */
+char *_strdup(char *str)
+{
+	char *t;
+	int i = 0;
+	int s = strlen(str);
+
+	t = malloc(s * sizeof(char));
+
+	if (s == 0 || str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (i <= s)
+	{
+		t[i] = str[i];
+		i++;
+	}
+
+	return (t);
+}
