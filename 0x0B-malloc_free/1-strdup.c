b9dff775883c230a53f0f13bc958a0e43efbee67
Rayd Trujillo <rayd1893@gmail.com>
2021-07-14 03:46:21 +0000

    Print String

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,27 @@
 #include <stdlib.h>
-#include <string.h>
 #include "holberton.h"
+
+/**
+ * _strlen - String length
+ * @s: String
+ * Return: Length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (s == NULL)
+	{
+		return (0);
+	}
+
+	while (*s)
+	{
+		i++;
+	}
+
+	return (i);
+}
 
 /**
  * _strdup - Return string
@@ -11,16 +32,20 @@ char *_strdup(char *str)
 {
 	char *t;
 	int i = 0;
-	int s = strlen(str);
 
-	t = malloc(s + 1);
+	t = (char *)malloc(_strlen(str) + 1);
 
-	if (str == NULL || t == NULL)
+	if (str == NULL)
 	{
 		return (NULL);
 	}
 
-	while (i <= s)
+	if (t == NULL)
+	{
+		return (NULL);
+	}
+
+	while (i < _strlen(str))
 	{
 		t[i] = str[i];
 		i++;
