34c882c97829692159aba97e34e0016da7d54ad9
Rayd Trujillo <rayd1893@gmail.com>
2021-07-15 05:06:10 +0000

    Create matriz

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -21,7 +21,16 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
+	{
 		grid[i] = malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+		{
+			free(grid);
+			for (j = 0; j <= i; j++)
+				free(grid[j]);
+			return (NULL);
+		}
+	}
 
 	for (i = 0; i < height; i++)
 	{
