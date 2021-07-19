0a27b259d29b19a5fe5abea7c1c3d2354dbdfe72
Rayd Trujillo <rayd1893@gmail.com>
2021-07-19 20:15:45 +0000

    First struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef GRANDPARENT_H
 #define GRANDPARENT_H
 
-struct dog {
+/**
+ * struct dog - Create struct dog
+ * @name: Dog´s name
+ * @age: Dog´s age
+ * @owner: Dog´s owner
+ *
+ * Description: Create struct dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
