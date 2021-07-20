df235550716e622ccfba6715172c570343118099
Rayd Trujillo <rayd1893@gmail.com>
2021-07-20 03:43:33 +0000

    Print struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,7 +14,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
 
-		printf("Age: %.1f\n", d->age);
+		printf("Age: %.6f\n", d->age);
 
 		printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 	}
