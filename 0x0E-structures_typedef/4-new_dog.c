9169fc9642571e7233102a5c86e6ca2d62e03e8b
Rayd Trujillo <rayd1893@gmail.com>
2021-07-20 04:47:13 +0000

    Set struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,7 +11,9 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-        dog_t *my_dog = malloc(sizeof(*my_dog));
+	dog_t *my_dog;
+
+	my_dog = malloc(sizeof(*my_dog));
 
 	if (my_dog == NULL)
 		return (NULL);
