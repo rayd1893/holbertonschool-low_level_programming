01a95a538d623cc57c0d4bf13ab4ff95b5af22b7
Rayd Trujillo <rayd1893@gmail.com>
2021-08-06 06:12:24 +0000

    Print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,39 +1,5 @@
 #include "main.h"
 
-/**
- * division - Divide two number
- * @a: First number
- * @b: Second number
- *
- * Return: Division
- */
-int division(int a, int b)
-{
-	int count = 0;
-
-	while (a >= b)
-	{
-		a -= b;
-		count++;
-	}
-
-	return (count);
-}
-
-/**
- * residue - Divide two number
- * @a: First number
- * @b: Second number
- *
- * Return: Residue
- */
-int residue(int a, int b)
-{
-	while (a >= b)
-		a -= b;
-
-	return (a);
-}
 
 /**
  * print_binary - Print binary from decimal number
@@ -43,16 +9,8 @@ int residue(int a, int b)
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int a;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	if (n < 2)
-	{
-		_putchar((n & 1) + '0');
-		return;
-	}
-
-	a = residue(n, 2);
-	n = division(n, 2);
-	print_binary(n);
-	_putchar('0' + a);
+	_putchar((n & 1) + '0');
 }
