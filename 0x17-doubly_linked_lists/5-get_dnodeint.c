19b4270caa7b14d956ec3107d9f3090d49aadf8c
Rayd Trujillo <rayd1893@gmail.com>
2021-09-17 04:03:44 +0000

    Betty style

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (head)
 	{
 		h = head;
-	        if (i == index)
+		if (i == index)
 			return (h);
 		i++;
 		head = head->next;
