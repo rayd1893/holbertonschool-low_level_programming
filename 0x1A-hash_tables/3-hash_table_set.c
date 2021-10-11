d606fd47204bcb86e1a43590e423841b4eff975e
Rayd Trujillo <rayd1893@gmail.com>
2021-10-11 01:36:53 +0000

    Hash table set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Add key and value in hash_table
+ * @ht: Hash Table
+ * @key: Key
+ * @value: Value
+ *
+ * Return: Success or not
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = key_index((unsigned char *) key, ht->size);
+
+	ht->array[index]->key = (char *)key;
+	ht->array[index]->value = (char *)value;
+	ht->array[index]->next = NULL;
+
+	return (1);
+}
