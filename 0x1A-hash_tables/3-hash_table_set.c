d4316228e353b5835ccdc5d9b21a737cd7901fe5
Rayd Trujillo <rayd1893@gmail.com>
2021-10-11 03:42:45 +0000

    Set hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,14 +10,37 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *node;
+	hash_node_t *new;
 	unsigned long int index = key_index((unsigned char *) key, ht->size);
 
 	if (strlen(key) == 0  || key == NULL)
 		return (0);
 
-	ht->array[index]->key = (char *)key;
-	ht->array[index]->value = (char *)value;
-	ht->array[index]->next = NULL;
+	node = ht->array[index];
+
+	while (node)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			strcpy(node->value, value);
+			return (1);
+		}
+		node = node->next;
+	}
+
+	new = malloc(sizeof(hash_node_t));
+
+	if (new == NULL)
+	{
+		return (0);
+	}
+
+	strcpy(new->key, key);
+	strcpy(new->value, value);
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
 	return (1);
 }
