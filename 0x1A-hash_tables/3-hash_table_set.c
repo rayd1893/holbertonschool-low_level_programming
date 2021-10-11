52e3255d9200545e91cfb60e48ddedb997ab57b3
Rayd Trujillo <rayd1893@gmail.com>
2021-10-11 01:47:48 +0000

    fixed function

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = key_index((unsigned char *) key, ht->size);
 
-	if (key == "" || key == NULL)
+	if (strlen(key) == 0  || key == NULL)
 		return (0);
 
 	ht->array[index]->key = (char *)key;
